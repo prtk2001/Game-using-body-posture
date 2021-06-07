72baea504beefc81afd08c6244bc1ffb88558a32
prtk2001 <prtk2001@gmail.com>
2021-06-07 12:51:02 +0530

    update
    Added pose detection from previous project and added box with a stationary ball in it(which needs to move)

--- bodygame_V_01.py
+++ bodygame_V_01.py
@@ -55,7 +55,207 @@ score = 0
 ball_1 = canvas.create_oval(50, 50, 100, 100, fill = "black")
 rect = canvas.create_rectangle(stick_height, stick_height, WIDTH - stick_height, HEIGHT - stick_height)
 text = canvas.create_text(WIDTH*4/5, 10, fill="darkblue", font="Times 20 italic bold", text="Player: {}".format(player_name))
+## Setup mediapipe instance --------------------------------------------------------------------------------------------
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+  while cap.isOpened():
+    ret, frame = cap.read()
 
-canvas.mainloop()
-cap.release()
-cv2.destroyAllWindows()
+    # Recolor image to RGB
+    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    image.flags.writeable = False
+
+    # Make detection
+    results = pose.process(image)
+
+    # Recolor back to BGR
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    head = [-300, -300]
+    R_hand_mid = [-300, -300]
+    R_hand_end = [-300, -300]
+    L_hand_mid = [-300, -300]
+    L_hand_end = [-300, -300]
+    R_leg_mid = [-300, -300]
+    R_leg_end = [-300, -300]
+    L_leg_mid = [-300, -300]
+    L_leg_end = [-300, -300]
+
+    # Extract landmarks ------------------------------------------------------------------------------------------------
+    try:
+      landmarks = results.pose_landmarks.landmark
+
+      head = [WIDTH * (1-landmarks[mp_pose.PoseLandmark.NOSE.value].x), HEIGHT * landmarks[mp_pose.PoseLandmark.NOSE.value].y]
+
+      L_hand_mid = [WIDTH *(1- landmarks[mp_pose.PoseLandmark.LEFT_ELBOW].x), HEIGHT * landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+      L_hand_end = [WIDTH * (1-landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x), HEIGHT * landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+
+      R_hand_mid = [WIDTH * (1-landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW].x), HEIGHT * landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+      R_hand_end = [WIDTH * (1-landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x), HEIGHT * landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+
+      L_leg_mid = [WIDTH * (1-landmarks[mp_pose.PoseLandmark.LEFT_KNEE].x), HEIGHT * landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]
+      L_leg_end = [WIDTH * (1-landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x), HEIGHT * landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
+
+      R_leg_mid = [WIDTH * (1-landmarks[mp_pose.PoseLandmark.RIGHT_KNEE].x), HEIGHT * landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
+      R_leg_end = [WIDTH * (1-landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].x), HEIGHT * landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
+
+    except:
+      pass
+
+    # Render detections ------------------------------------------------------------------------------------------------
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
+                              mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+                              )
+    # Head -------------------------------------------------------------------------------------------------------------
+    head_r = head_size / 2
+
+    head_x1 = head[0] - head_r
+    head_y1 = head[1] - head_r
+    head_x2 = head_x1 + head_size
+    head_y2 = head_y1 + head_size
+
+    head_center_x = head_x1 + head_r
+    head_center_y = head_y1 + head_r
+
+    # Smile ------------------------------------------------------------------------------------------------------------
+    smile_x1 = head_center_x - 10
+    smile_y1 = head_center_y + head_r/2
+    smile_x2 = head_center_x + 10
+    smile_y2 = head_center_y + head_r/2 + 10
+
+    # Neck -------------------------------------------------------------------------------------------------------------
+    neck_mid_x = head_center_x
+    neck_mid_y = head_center_y + head_r
+
+    neck_x1 = neck_mid_x - 5
+    neck_y1 = neck_mid_y
+    neck_x2 = neck_mid_x + 5
+    neck_y2 = neck_mid_y + neck_size
+
+    # Eyes -------------------------------------------------------------------------------------------------------------
+    L_eye_x2 = head_center_x - head_r / 2
+    L_eye_y2 = head_center_y
+    L_eye_x1 = L_eye_x2 - eye_size
+    L_eye_y1 =L_eye_y2 - eye_size
+
+    R_eye_x2 = head_center_x + head_r / 2
+    R_eye_y2 = head_center_y
+    R_eye_x1 = R_eye_x2 + eye_size
+    R_eye_y1 = R_eye_y2 - eye_size
+
+    # Sholder ----------------------------------------------------------------------------------------------------------
+    sholder_mid_x = head_center_x
+    sholder_mid_y = head_center_y + head_r + neck_size
+    sholder_x1 = sholder_mid_x - (sholder_length / 2)
+    sholder_y1 = sholder_mid_y
+    sholder_x2 = sholder_mid_x + (sholder_length / 2)
+    sholder_y2 = sholder_mid_y
+
+    # Body -------------------------------------------------------------------------------------------------------------
+    body_y2 = sholder_y2 + body_height
+
+    # Hands ------------------------------------------------------------------------------------------------------------
+    L_hand_x = sholder_x1
+    L_hand_y = sholder_y1
+    R_hand_x = sholder_x2
+    R_hand_y = sholder_y2
+
+    L_hand_mid_x = L_hand_mid[0]
+    L_hand_mid_y = L_hand_mid[1]
+    L_hand_end_x = L_hand_end[0]
+    L_hand_end_y = L_hand_end[1]
+
+    R_hand_mid_x = R_hand_mid[0]
+    R_hand_mid_y = R_hand_mid[1]
+    R_hand_end_x = R_hand_end[0]
+    R_hand_end_y = R_hand_end[1]
+
+    # Hand Fingers -----------------------------------------------------------------------------------------------------
+    Lh_fingure_x1 = L_hand_end_x - fingure_size
+    Lh_fingure_y1 = L_hand_end_y - fingure_size
+    Lh_fingure_x2 = L_hand_end_x + fingure_size
+    Lh_fingure_y2 = L_hand_end_y + fingure_size
+
+    Rh_fingure_x1 = R_hand_end_x - fingure_size
+    Rh_fingure_y1 = R_hand_end_y - fingure_size
+    Rh_fingure_x2 = R_hand_end_x + fingure_size
+    Rh_fingure_y2 = R_hand_end_y + fingure_size
+
+    # Legs -------------------------------------------------------------------------------------------------------------
+    L_leg_x = sholder_x1
+    L_leg_y = sholder_y1 + body_height
+
+    R_leg_x = sholder_x2
+    R_leg_y = sholder_y2 + body_height
+
+    L_leg_mid_x = L_leg_mid[0]
+    L_leg_mid_y = L_leg_mid[1]
+    L_leg_end_x = L_leg_end[0]
+    L_leg_end_y = L_leg_end[1]
+
+    R_leg_mid_x = R_leg_mid[0]
+    R_leg_mid_y = R_leg_mid[1]
+    R_leg_end_x = R_leg_end[0]
+    R_leg_end_y = R_leg_end[1]
+
+    # Leg Fingers ------------------------------------------------------------------------------------------------------
+    Ll_fingure_x1 = L_leg_end_x - fingure_size
+    Ll_fingure_y1 = L_leg_end_y - fingure_size
+    Ll_fingure_x2 = L_leg_end_x + fingure_size
+    Ll_fingure_y2 = L_leg_end_y + fingure_size
+
+    Rl_fingure_x1 = R_leg_end_x - fingure_size
+    Rl_fingure_y1 = R_leg_end_y - fingure_size
+    Rl_fingure_x2 = R_leg_end_x + fingure_size
+    Rl_fingure_y2 = R_leg_end_y + fingure_size
+
+    if draw_man == TRUE:
+      head = canvas.create_oval(head_x1, head_y1, head_x2, head_y2, fill="pink")
+      hairs = canvas.create_arc(head_x1, head_y1, head_x2, head_y2 - 3 * eye_size, start=0, extent=180, fill="black")
+      smile = canvas.create_arc(smile_x1, smile_y1, smile_x2, smile_y2, start=0, extent=-180, fill="black")
+
+      neck = canvas.create_rectangle(neck_x1, neck_y1, neck_x2, neck_y2)
+      L_eye = canvas.create_oval(L_eye_x1, L_eye_y1, L_eye_x2, L_eye_y2, fill="blue")
+      R_eye = canvas.create_oval(R_eye_x1, R_eye_y1, R_eye_x2, R_eye_y2, fill="blue")
+
+      body = canvas.create_rectangle(sholder_x1, sholder_y1, sholder_x2, body_y2)
+
+      Left_hand_mid = canvas.create_line(L_hand_x, L_hand_y, L_hand_mid_x, L_hand_mid_y)
+      Left_hand_end = canvas.create_line(L_hand_mid_x, L_hand_mid_y, L_hand_end_x, L_hand_end_y)
+      Right_hand_mid = canvas.create_line(R_hand_x, R_hand_y, R_hand_mid_x, R_hand_mid_y)
+      Right_hand_end = canvas.create_line(R_hand_mid_x, R_hand_mid_y, R_hand_end_x, R_hand_end_y)
+
+      Lh_fingures = canvas.create_oval(Lh_fingure_x1, Lh_fingure_y1, Lh_fingure_x2, Lh_fingure_y2, fill="black")
+      Rh_fingures = canvas.create_oval(Rh_fingure_x1, Rh_fingure_y1, Rh_fingure_x2, Rh_fingure_y2, fill="black")
+
+      Left_leg_mid = canvas.create_line(L_leg_x, L_leg_y, L_leg_mid_x, L_leg_mid_y)
+      Left_leg_end = canvas.create_line(L_leg_mid_x, L_leg_mid_y, L_leg_end_x, L_leg_end_y)
+      Right_leg_mid = canvas.create_line(R_leg_x, R_leg_y, R_leg_mid_x, R_leg_mid_y)
+      Right_leg_end = canvas.create_line(R_leg_mid_x, R_leg_mid_y, R_leg_end_x, R_leg_end_y)
+
+      Ll_fingures = canvas.create_oval(Ll_fingure_x1, Ll_fingure_y1, Ll_fingure_x2, Ll_fingure_y2, fill="black")
+      Rl_fingures = canvas.create_oval(Rl_fingure_x1, Rl_fingure_y1, Rl_fingure_x2, Rl_fingure_y2, fill="black")
+
+      # Body parts -----------------------------------------------------------------------------------------------------
+      Body_parts = [head, hairs, smile, neck, L_eye, R_eye, body, Left_hand_mid, Left_hand_end, Right_hand_mid,
+                  Right_hand_end, Lh_fingures, Rh_fingures, Left_leg_mid, Left_leg_end, Right_leg_mid,
+                  Right_leg_end, Ll_fingures, Rl_fingures]
+      
+      
+    
+
+    
+
+
+     # Flip and show image ------------------------------------------------------------------------------------------------
+    image = cv2.flip(image, 1)
+    cv2.imshow('Game', image)
+
+    # Close ------------------------------------------------------------------------------------------------------------
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+      break
+
+  canvas.mainloop()
+  cap.release()
+  cv2.destroyAllWindows()
